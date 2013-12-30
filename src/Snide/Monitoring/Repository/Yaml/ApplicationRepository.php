6132bc42a57251e9033993e5d2c9c846215f0071
Pascal DENIS <pascal.denis@businessdecision.com>
2013-12-30 21:51:51 +0100

    add licence

--- src/Snide/Monitoring/Repository/Yaml/ApplicationRepository.php
+++ src/Snide/Monitoring/Repository/Yaml/ApplicationRepository.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Snide monitoring package.
+ *
+ * (c) Pascal DENIS <pascal.denis.75@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Snide\Monitoring\Repository\Yaml;
 
 use Snide\Monitoring\Model\Application;
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Snide monitoring package.
+ *
+ * (c) Pascal DENIS <pascal.denis.75@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 $baseDir = dirname(__DIR__.'/../');
 $loader = require __DIR__.'/../vendor/autoload.php';
 $loader->add('Snide\\Monitoring', array($baseDir.'/src/', $baseDir.'/tests/'));
